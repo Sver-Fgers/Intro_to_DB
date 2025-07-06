afb38babfe06448bdeef4c97b1176ab71dccf70a
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-07-06 20:54:11 +0100

    modified exception handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,7 +17,7 @@ try:
 
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"❌ Failed to connect or create database: {e}")
 
 finally:
